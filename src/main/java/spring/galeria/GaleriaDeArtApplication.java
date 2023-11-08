1114b295ba1051f2273502026d182c8eeb95e64d
miriamelisa <miriam.guanolema1@gmail.com>
2023-11-08 16:59:42 +0100

    paquete vacio

---
+++ src/main/java/spring/galeria/GaleriaDeArtApplication.java
@@ -0,0 +1,13 @@
+package spring.galeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaleriaDeArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaleriaDeArtApplication.class, args);
+	}
+
+}
